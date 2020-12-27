9b33fadfd16826c4f1c8d819b0aca680cd21edfe
agent-whisper <fariz.tumbuan@gmail.com>
2020-12-27 11:54:16 +0800

    Refactored to follow pylint

--- owa_discordbot/client.py
+++ owa_discordbot/client.py
@@ -1,12 +1,18 @@
 import csv
+import random
 from pathlib import Path
-from typing import List, Dict
+from typing import List
 
-import discord, random
+import discord
 
 class OwaClient(discord.Client):
-    def __init__(self, *args, config={}, **kwargs):
+    """
+    Subclass of discord client for Owa Bot.
+    """
+    def __init__(self, config=None):
         super().__init__()
+        if config is None:
+            raise ValueError("No config provided")
         self._config = config
         self._prefix = self._config["prefix"]
         self._questions: List[str] = []
@@ -22,20 +28,27 @@ class OwaClient(discord.Client):
                 self._questions.append(row[0])
 
     async def on_ready(self):
-        print('Logged on as {0}!'.format(self.user))
+        """
+        Client behavior on successful deployment.
+        """
+        print(f"Logged in as {self.user}!")
 
     async def on_message(self, message):
+        """
+        Handle user inputs.
+        """
         if message.content.startswith(self._prefix):
             cmd = message.content.split(self._prefix)[1]
-            if cmd == "q" or cmd == "question":
-                # generate question
+            if cmd in ["q", "question"]:
                 picked_question = random.choice(self._questions)
                 await message.channel.send(picked_question)
-            # TODO
-            # elif cmd == "wyr":
-            #     # would you rather
-            elif cmd == "help" or cmd == "h":
-                await message.channel.send(f"-- OwaOwa Bot v0.1 --\nPrefix: `{self._prefix}`\n\nCommand:\n`q` for random question\n`h` for help\n\nExample: `{self._prefix}help`")
+            elif cmd in ["help", "h"]:
+                await message.channel.send(
+                    f"-- OwaOwa Bot v0.1 --\n"
+                    f"Prefix: `{self._prefix}`\n\n"
+                    f"Command:\n`q` for random question\n`h` for help\n\n"
+                    f"Example: `{self._prefix}help`"
+                )
 
     def run(self,  *args, **kwargs):
         return super().run(self._discord_token, *args, **kwargs)
