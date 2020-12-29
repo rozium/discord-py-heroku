6fb500f207e26094fac55a768bb0d452bc4b8bef
agent-whisper <fariz.tumbuan@gmail.com>
2020-12-30 06:56:49 +0800

    Init database module

--- owa_discordbot/settings.py
+++ owa_discordbot/settings.py
@@ -7,8 +7,12 @@ load_dotenv()
 env_path = Path(".", ".env")
 load_dotenv(env_path)
 
+DEBUG = os.getenv("OWA_DEBUG", default="true").lower() == "true"
+
 OWA_CONFIG = {
     "prefix": os.getenv("OWA_PREFIX"),
     "discord_token": os.getenv("OWA_DISCORD_TOKEN"),
     "csv_dir": os.getenv("OWA_QUESTIONS_CSV"),
 }
+
+DB_HOST = os.getenv("DB_HOST")
