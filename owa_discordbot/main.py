3e81d12f4b62c622d37d100b490bde1307aca512
agent-whisper <fariz.tumbuan@gmail.com>
2020-12-30 10:00:54 +0800

    Refactored client and main to use db interface

--- owa_discordbot/main.py
+++ owa_discordbot/main.py
@@ -1,5 +1,6 @@
 from owa_discordbot import settings
 from owa_discordbot.client import OwaClient
+from owa_discordbot.database import get_session
 
-client = OwaClient(config=settings.OWA_CONFIG)
+client = OwaClient(config=settings.OWA_CONFIG, db=get_session())
 client.run()
